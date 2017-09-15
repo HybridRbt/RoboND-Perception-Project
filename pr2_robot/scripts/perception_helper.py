ff38ba8fefb204778f50fcc66820d8e70fb3f394
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-09-15 16:14:27 -0700

    add functions for clustering

--- pr2_robot/scripts/perception_helper.py
+++ pr2_robot/scripts/perception_helper.py
@@ -1,3 +1,59 @@
+def clustering(pcl_data):
+    # Create k-d tree
+    white_cloud = XYZRGB_to_XYZ(pcl_data)
+    tree = white_cloud.make_kdtree()
+
+    # Create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+
+    # Set tolerances for distance threshold
+    # 0.001: too small, the cluster will be too fractrized, colored like rainbow
+    # 0.01: less colorful, but still mixing multiple colors onto one object
+    # 0.1: nothing will show up except for the bowl at the center
+    # this means the min and max numbers need to be tweaked, more specifically,
+    # increase maximum
+    # when it was increased to 5000, all the objects are shown, now can get back
+    # to tweak this number between 0.01 and 0.1
+    # a unique color is achived for each object when this number = 0.014
+    ec.set_ClusterTolerance(0.014)
+
+    # Set minimum cluster size
+    # the lower this number, the more points each cluster has
+    # but it cannot be too small otherwise there will be "noises" inside each
+    # object
+    ec.set_MinClusterSize(5)
+
+    # Set maximum cluster size
+    # the larger this number, the more ponints will show up
+    # but it cannot be too large otherwise two objects may be colored into one
+    ec.set_MaxClusterSize(10000)
+
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(tree)
+
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
+    return white_cloud, tree, cluster_indices
+
+def color_clusters(pcl_data_xyz, cluster_indices):
+    # Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+
+    # create a list for the colored cluster points
+    color_cluster_point_list = []
+
+    # traverse the indices and append to the list
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([pcl_data_xyz[indice][0],
+                                             pcl_data_xyz[indice][1],
+                                             pcl_data_xyz[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+    # Create new cloud containing all clusters colored
+    cluster_cloud_colored = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud_colored.from_list(color_cluster_point_list)
+    return cluster_cloud_colored
+
 def statistical_outlier_fiter(pcl_data):
     # Start by creating a filter object:
     outlier_filter = cloud_filtered.make_statistical_outlier_filter()
