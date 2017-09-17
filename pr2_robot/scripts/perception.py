ad802d5c1fcd20f137195428784163f8c8ba8347
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-09-17 10:17:06 -0700

    output to yaml

--- pr2_robot/scripts/perception.py
+++ pr2_robot/scripts/perception.py
@@ -275,6 +275,10 @@ def pr2_mover(object_list):
         #except rospy.ServiceException, e:
         #    print "Service call failed: %s"%e
 
+    # Output your request parameters into output yaml file
+    print "now ouput to yaml"
+    yaml_filename = 'output_1.yaml'
+    send_to_yaml(yaml_filename, dict_list)
 
 
 if __name__ == '__main__':
